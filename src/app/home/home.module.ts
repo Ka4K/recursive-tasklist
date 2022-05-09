9d6b031e72e47d6e62767c0a47db6a37b11709a6
Ka4K <kawayann1@gmail.com>
2022-05-09 15:22:54 +0900

    feat(task):根タスクの追加の実装

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,12 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { ShowTasksComponent } from '../shared/show-tasks/show-tasks.component';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, ShowTasksComponent],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITask } from '../interface/task';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  tasks: ITask[] = [
+    { name: '1', children: [] },
+    {
+      name: '2',
+      children: [
+        { name: '3', children: [] },
+        { name: '4', children: [{ name: '5', children: [] }] },
+      ],
+    },
+  ];
+  task: string;
   constructor() {}
-
+  addTask() {
+    this.tasks.push({ name: this.task, children: [] });
+  }
 }
