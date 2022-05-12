b2b4058fcf5e09eab85846dd8bd0d1d0cb2a511f
Ka4K <kawayann1@gmail.com>
2022-05-12 10:21:22 +0900

    feat(setting):設定ページのモックアップの作成

--- src/app/setting/setting.page.ts
+++ src/app/setting/setting.page.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./setting.page.scss'],
 })
 export class SettingPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
