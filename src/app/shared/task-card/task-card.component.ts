174f6321f86b9451d008c902646706a35f505f93
Ka4K <kawayann1@gmail.com>
2022-05-12 13:32:33 +0900

    refactor(task-card):インターフェースの変更

--- src/app/shared/task-card/task-card.component.ts
+++ src/app/shared/task-card/task-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ITask } from 'src/app/interface/task';
 
 @Component({
   selector: 'app-task-card',
@@ -6,8 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent implements OnInit {
-  @Input() name: string;
-  @Input() duedate: string;
+  @Input() task: ITask;
   constructor() {}
 
   ngOnInit() {}
