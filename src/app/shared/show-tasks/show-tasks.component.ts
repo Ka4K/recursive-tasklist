be11837b435ae2fb596ee601818fd8af1de17b00
Ka4K <kawayann1@gmail.com>
2022-05-13 14:57:20 +0900

    refactor(task-card):編集ボタンを追加

--- src/app/shared/show-tasks/show-tasks.component.ts
+++ src/app/shared/show-tasks/show-tasks.component.ts
@@ -1,14 +1,6 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { ITask, deleteChild } from 'src/app/interface/task';
-import {
-  ActionSheetController,
-  AlertController,
-  ModalController,
-  IonAccordionGroup,
-} from '@ionic/angular';
-import { UpdateTaskModalComponent } from 'src/app/update-task-modal/update-task-modal.component';
-import { MakeChildModalComponent } from 'src/app/make-child-modal/make-child-modal.component';
-import { TaskProgressManagerService } from '../task-progress-manager.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ITask } from 'src/app/interface/task';
+
 @Component({
   selector: 'app-show-tasks',
   templateUrl: './show-tasks.component.html',
@@ -16,80 +8,9 @@ import { TaskProgressManagerService } from '../task-progress-manager.service';
 })
 export class ShowTasksComponent implements OnInit {
   @Input() task: ITask;
-  @ViewChild(IonAccordionGroup, { static: true })
-  accordionGroup: IonAccordionGroup;
-  constructor(
-    public actionSheetController: ActionSheetController,
-    public alertController: AlertController,
-    public modalCtrl: ModalController,
-    public taskManager: TaskProgressManagerService
-  ) {}
-  async changeTask(index: number, $event) {
-    $event.stopPropagation();
-    $event.preventDefault();
-    const actionSheet = await this.actionSheetController.create({
-      header: 'タスクの変更',
-      buttons: [
-        {
-          text: '完了',
-          icon: 'checkbox',
-          handler: () => {
-            this.taskManager.increseComplete(this.task.children[index].duedate);
-            deleteChild(this.task, index);
-          },
-        },
-        {
-          text: '削除',
-          role: 'destructive',
-          icon: 'trash',
-          handler: () => {
-            deleteChild(this.task, index);
-          },
-        },
-        {
-          text: '子タスクの追加',
-          icon: 'create',
-          handler: () => {
-            this._createChild(index);
-          },
-        },
-        {
-          text: '変更',
-          icon: 'sync',
-          handler: () => {
-            this._renameTask(index);
-          },
-        },
-        {
-          text: '閉じる',
-          icon: 'close',
-          role: 'cancel',
-          handler: () => {},
-        },
-      ],
-    });
-    actionSheet.present();
-  }
+  constructor() {}
   ngOnInit() {}
   hasChildren(t: ITask) {
     return t.children.length;
-  }
-  async _renameTask(index: number) {
-    const modal = await this.modalCtrl.create({
-      component: UpdateTaskModalComponent,
-      componentProps: {
-        task: this.task.children[index],
-      },
-    });
-    await modal.present();
-  }
-  async _createChild(index: number) {
-    const modal = await this.modalCtrl.create({
-      component: MakeChildModalComponent,
-      componentProps: {
-        parent: this.task.children[index],
-      },
-    });
-    await modal.present();
   }
 }
--- src/app/shared/task-card/task-card.component.ts
+++ src/app/shared/task-card/task-card.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ITask } from 'src/app/interface/task';
-
+import {
+  ActionSheetController,
+  AlertController,
+  ModalController,
+} from '@ionic/angular';
+import { ITask, deleteChild } from 'src/app/interface/task';
+import { TaskProgressManagerService } from '../task-progress-manager.service';
+import { UpdateTaskModalComponent } from 'src/app/update-task-modal/update-task-modal.component';
+import { MakeChildModalComponent } from 'src/app/make-child-modal/make-child-modal.component';
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
@@ -10,7 +17,82 @@ export class TaskCardComponent implements OnInit {
   @Input() task: ITask;
   @Input() highlight: boolean = false;
   @Input() showRecent: boolean = false;
-  constructor() {}
+  @Input() edit: boolean = false;
+  @Input() parent: ITask | undefined;
+  @Input() index: number | undefined;
 
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController,
+    public modalCtrl: ModalController,
+    public taskManager: TaskProgressManagerService
+  ) {}
+  async changeTask($event) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '完了',
+          icon: 'checkbox',
+          handler: () => {
+            this.taskManager.increseComplete(this.task.duedate);
+            deleteChild(this.parent, this.index);
+          },
+        },
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            deleteChild(this.parent, this.index);
+          },
+        },
+        {
+          text: '子タスクの追加',
+          icon: 'create',
+          handler: () => {
+            this._createChild();
+          },
+        },
+        {
+          text: '変更',
+          icon: 'sync',
+          handler: () => {
+            this._renameTask();
+          },
+        },
+        {
+          text: '閉じる',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {},
+        },
+      ],
+    });
+    actionSheet.present();
+  }
   ngOnInit() {}
+  hasChildren(t: ITask) {
+    return t.children.length;
+  }
+  async _renameTask() {
+    const modal = await this.modalCtrl.create({
+      component: UpdateTaskModalComponent,
+      componentProps: {
+        task: this.task,
+      },
+    });
+    await modal.present();
+  }
+  async _createChild() {
+    const modal = await this.modalCtrl.create({
+      component: MakeChildModalComponent,
+      componentProps: {
+        parent: this.task,
+      },
+    });
+    await modal.present();
+  }
 }
