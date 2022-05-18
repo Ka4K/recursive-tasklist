62c3f83ae5dd5df7b32262b536a69e254af5b975
Ka4K <kawayann1@gmail.com>
2022-05-18 15:29:13 +0900

    feat(statistics):スマホのUIの設定

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CalendarData, CalendarOptions } from 'ng-calendar-heatmap';
 import { Platform } from '@ionic/angular';
 import { TaskProgressService } from '../shared/task-progress.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-statistics',
@@ -14,11 +15,19 @@ export class StatisticsPage implements OnInit {
     responsive: true,
     tooltipEnabled: false,
   };
+  isMobile: boolean;
   constructor(
     private platform: Platform,
     private taskProgress: TaskProgressService
   ) {
-    this.calendarOptions.responsive = !platform.is('mobile');
+    this.isMobile = platform.is('mobile') && !platform.is('tablet');
+    if (this.isMobile) {
+      this.calendarOptions.yearAgo = moment()
+        .startOf('day')
+        .subtract(4, 'month')
+        .toDate();
+      this.calendarOptions.responsive = false;
+    }
     this.calendarData = taskProgress.getCalendarData();
   }
   ionViewWillEnter() {}
