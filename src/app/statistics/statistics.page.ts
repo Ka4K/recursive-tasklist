886790f7a3395eacf94fcc68e537cb64e8c019de
Ka4K <kawayann1@gmail.com>
2022-05-19 14:08:11 +0900

    change(statistics):草カレンダーの表示方法の変更

--- src/app/statistics/statistics.page.ts
+++ src/app/statistics/statistics.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarData, CalendarOptions } from 'ng-calendar-heatmap';
-import { Platform } from '@ionic/angular';
 import { TaskProgressService } from '../shared/task-progress.service';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-statistics',
@@ -12,22 +10,10 @@ import * as moment from 'moment';
 export class StatisticsPage implements OnInit {
   calendarData: CalendarData[] = [];
   calendarOptions: CalendarOptions = {
-    responsive: true,
     tooltipEnabled: false,
   };
   isMobile: boolean;
-  constructor(
-    private platform: Platform,
-    private taskProgress: TaskProgressService
-  ) {
-    this.isMobile = platform.is('mobile') && !platform.is('tablet');
-    if (this.isMobile) {
-      this.calendarOptions.yearAgo = moment()
-        .startOf('day')
-        .subtract(4, 'month')
-        .toDate();
-      this.calendarOptions.responsive = false;
-    }
+  constructor(private taskProgress: TaskProgressService) {
     this.calendarData = taskProgress.getCalendarData();
   }
   ionViewWillEnter() {}
