2d4a8e4d57510450c2b29e73f5d32a64961a4521
Ka4K <kawayann1@gmail.com>
2022-05-24 11:32:27 +0900

    change(show-tasks):スワイプで削除できるように

--- src/app/shared/show-tasks/show-task-body/show-task-body.component.ts
+++ src/app/shared/show-tasks/show-task-body/show-task-body.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ITask } from 'src/app/interface/task';
+import { TaskProgressService } from '../../task-progress.service';
+import { UpdateTaskModalComponent } from 'src/app/shared/update-task-modal/update-task-modal.component';
+import { MakeChildModalComponent } from 'src/app/shared/make-child-modal/make-child-modal.component';
+import { TaskService } from '../../task.service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-show-task-body',
@@ -9,10 +14,49 @@ import { ITask } from 'src/app/interface/task';
 export class ShowTaskBodyComponent implements OnInit {
   @Input() index: number;
   @Input() task: ITask;
-  constructor() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private taskManager: TaskProgressService,
+    private taskServise: TaskService
+  ) {}
 
   ngOnInit(): void {}
+  deleteTask($event): void {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.taskServise.deleteChild(this.task.parent, this.index);
+  }
+  createChild($event): void {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.modalCtrl
+      .create({
+        component: MakeChildModalComponent,
+        componentProps: {
+          parent: this.task,
+        },
+      })
+      .then((modal) => modal.present());
+  }
   hasChildren(t: ITask): boolean {
     return !!t.children.length;
+  }
+  taskComplete($event): void {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.taskManager.increseComplete(this.task.duedate);
+    this.taskServise.deleteChild(this.task.parent, this.index);
+  }
+  renameTask($event): void {
+    $event.stopPropagation();
+    $event.preventDefault();
+    this.modalCtrl
+      .create({
+        component: UpdateTaskModalComponent,
+        componentProps: {
+          task: this.task,
+        },
+      })
+      .then((modal) => modal.present());
   }
 }
--- src/app/shared/task-card/task-card.component.ts
+++ src/app/shared/task-card/task-card.component.ts
@@ -1,10 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActionSheetController, ModalController } from '@ionic/angular';
 import { ITask } from 'src/app/interface/task';
-import { TaskProgressService } from '../task-progress.service';
-import { UpdateTaskModalComponent } from 'src/app/shared/update-task-modal/update-task-modal.component';
-import { MakeChildModalComponent } from 'src/app/shared/make-child-modal/make-child-modal.component';
-import { TaskService } from '../task.service';
+
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
@@ -14,75 +10,8 @@ export class TaskCardComponent implements OnInit {
   @Input() task: ITask;
   @Input() highlight = false;
   @Input() showRecent = false;
-  @Input() edit = false;
   @Input() index: number | undefined;
 
-  constructor(
-    private actionSheetController: ActionSheetController,
-    private modalCtrl: ModalController,
-    private taskManager: TaskProgressService,
-    private taskServise: TaskService
-  ) {}
+  constructor() {}
   ngOnInit(): void {}
-  changeTask($event): void {
-    $event.stopPropagation();
-    $event.preventDefault();
-    this.actionSheetController
-      .create({
-        header: 'タスクの変更',
-        buttons: [
-          {
-            text: '変更',
-            icon: 'sync',
-            handler: () => this.renameTask(),
-          },
-          {
-            text: '削除',
-            role: 'destructive',
-            icon: 'trash',
-            handler: () => {
-              this.taskServise.deleteChild(this.task.parent, this.index);
-            },
-          },
-          {
-            text: '閉じる',
-            icon: 'close',
-            role: 'cancel',
-            handler: () => {},
-          },
-        ],
-      })
-      .then((actionSheet) => actionSheet.present());
-  }
-  createChild($event): void {
-    $event.stopPropagation();
-    $event.preventDefault();
-    this.modalCtrl
-      .create({
-        component: MakeChildModalComponent,
-        componentProps: {
-          parent: this.task,
-        },
-      })
-      .then((modal) => modal.present());
-  }
-  hasChildren(t: ITask): boolean {
-    return !!t.children.length;
-  }
-  taskComplete($event): void {
-    $event.stopPropagation();
-    $event.preventDefault();
-    this.taskManager.increseComplete(this.task.duedate);
-    this.taskServise.deleteChild(this.task.parent, this.index);
-  }
-  private renameTask(): void {
-    this.modalCtrl
-      .create({
-        component: UpdateTaskModalComponent,
-        componentProps: {
-          task: this.task,
-        },
-      })
-      .then((modal) => modal.present());
-  }
 }
